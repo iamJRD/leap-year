c36bd0239beaac9fc9b00244fc9ee1a166eee89a
erik tolentino and jared beckler <jared.beckler@gmail.com>
2016-01-19 09:25:31 -0800

    add files for leapYear layout

--- specs/specs.js
+++ specs/specs.js
@@ -11,4 +11,12 @@ describe('leapYear', function() {
     expect(leapYear(1900)).to.equal(false);
   });
 
+  it("is true for years divisable by 400", function() {
+    expect(leapYear(2000)).to.equal(true);
+  });
+
+  it("is false for inputs of text", function() {
+    expect(leapYear("word")).to.equal(false);
+  });
+
 });
